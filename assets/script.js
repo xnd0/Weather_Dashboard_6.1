5611f7c1ef695db0d5e571f34add4519a09795dd
xndcmd <xndcmd@gmail.com>
2022-06-11 10:17:36 -0700

    successfully prints Fetch Response to console

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,14 @@
 // ----- Javascript ----- //
 // ---------------------- //
 
+
+var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1d148820fae7548eecd9ed98012efbde";
+
+fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log('Fetch Response \n-------------');
+        console.log(data.city);
+    });
