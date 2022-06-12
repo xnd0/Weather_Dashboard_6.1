481d9666d725def2b5944d45f9a7fe4e333341cd
xndcmd <xndcmd@gmail.com>
2022-06-11 17:43:19 -0700

    successfully prints search input to console

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,24 @@
 // ----- Javascript ----- //
 // ---------------------- //
 
+var searchCityButton = document.querySelector('#searchCityButton');
+var cityInput = document.querySelector('#searchCity');
+
 
 var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1d148820fae7548eecd9ed98012efbde";
+
+
+
+function showResponse(event) {
+    // Prevent default action
+    event.preventDefault();
+
+    console.log('searchCity is:' + cityInput.value);
+
+  }
+
+
+
 
 fetch(requestUrl)
     .then(function (response) {
@@ -13,3 +29,7 @@ fetch(requestUrl)
         console.log('Fetch Response \n-------------');
         console.log(data.city);
     });
+
+
+searchCityButton.addEventListener("click", showResponse);
+
