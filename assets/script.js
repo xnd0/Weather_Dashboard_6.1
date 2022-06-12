6680aecdcc3d1b66534e964df253e53250a47365
xndcmd <xndcmd@gmail.com>
2022-06-11 18:18:21 -0700

    making progress with geocoding API response

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,9 @@ var cityResult = document.getElementById('citySearchResult');
 
 var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=1d148820fae7548eecd9ed98012efbde";
 
+// var requestCityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&limit=5&appid=1d148820fae7548eecd9ed98012efbde";
 
+// var requestCityUrl ="";
 
 function showResponse(event) {
     // Prevent default action
@@ -17,6 +19,17 @@ function showResponse(event) {
 
     console.log('searchCity is:' + cityInput.value);
     cityResult.textContent = cityInput.value;
+
+    requestCityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&limit=5&appid=1d148820fae7548eecd9ed98012efbde";
+
+    fetch(requestCityUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log('Fetch Response CITY GEOCODE \n-------------');
+        console.log(data);
+    });
 
   }
 
